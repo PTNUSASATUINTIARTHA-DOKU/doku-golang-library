2c081f1d5fc610d444ec4273b1db6e7397dccaf1
Robi Alhadhi <ahmad.robi83@gmail.com>
2024-10-21 15:28:56 +0700

    update models bankCardData and adjust createSignature function

--- models/directdebit/cardregistration/bankCardDataDTO.go
+++ models/directdebit/cardregistration/bankCardDataDTO.go
@@ -1,7 +1,10 @@
 package cardregistration
 
 type BankCardDataDTO struct {
-	BankCardNo   string `json:"bankCardNo"`
-	BankCardType string `json:"bankCardType"`
-	ExpiryDate   string `json:"expiryDate"`
+	BankCardNo         string `json:"bankCardNo"`
+	BankCardType       string `json:"bankCardType"`
+	IdentificationNo   string `json:"identificationNo,omitempty"`
+	IdentificationType string `json:"identificationType,omitempty"`
+	Email              string `json:"email,omitempty"`
+	ExpiryDate         string `json:"expiryDate"`
 }
