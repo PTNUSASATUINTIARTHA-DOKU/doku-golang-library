63827e161fa0f4e3ed984547309797d44924416b
Robi Alhadhi <ahmad.robi83@gmail.com>
2024-10-24 11:08:11 +0700

    change url endpoint

--- commons/config.go
+++ commons/config.go
@@ -1,7 +1,7 @@
 package commons
 
 const (
-	SANDBOX_BASE_URL                 = "https://api-uat.doku.com"
+	SANDBOX_BASE_URL                 = "https://api-sandbox.doku.com"
 	PRODUCTION_BASE_URL              = "https://dashboard.doku.com"
 	ACCESS_TOKEN                     = "/authorization/v1/access-token/b2b"
 	CREATE_VA                        = "/virtual-accounts/bi-snap-va/v1.1/transfer-va/create-va"
