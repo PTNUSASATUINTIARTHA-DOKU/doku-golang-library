4fb44f225a1576307991e9954c23d7cd4153a70c
rafidoku <104545377+rafidoku@users.noreply.github.com>
2024-11-20 09:30:37 +0700

    Fix Prod URL in SDK - GO

--- commons/config.go
+++ commons/config.go
@@ -2,7 +2,7 @@ package commons
 
 const (
 	SANDBOX_BASE_URL                 = "https://api-sandbox.doku.com"
-	PRODUCTION_BASE_URL              = "https://dashboard.doku.com"
+	PRODUCTION_BASE_URL              = "https://api.doku.com"
 	ACCESS_TOKEN                     = "/authorization/v1/access-token/b2b"
 	CREATE_VA                        = "/virtual-accounts/bi-snap-va/v1.1/transfer-va/create-va"
 	UPDATE_VA                        = "/virtual-accounts/bi-snap-va/v1.1/transfer-va/update-va"
