08f86b4dc07cddab16db756145610993768c135b
rafidoku <104545377+rafidoku@users.noreply.github.com>
2025-03-06 10:31:45 +0700

    [BE] SDK Enhancement Documentation and Add SDK Identifier 2 - Go
    [BE] SDK Enhancement Documentation and Add SDK Identifier 2 - Go

--- models/va/createVa/additionalInfo.go
+++ models/va/createVa/additionalInfo.go
@@ -1,6 +1,15 @@
 package models
 
+type Origin struct {
+	Product       string `json:"product,omitempty"`
+	Source        string `json:"source,omitempty"`
+	SourceVersion string `json:"sourceVersion,omitempty"`
+	System        string `json:"system,omitempty"`
+	ApiFormat     string `json:"apiFormat,omitempty"`
+}
+
 type AdditionalInfo struct {
 	Channel              string               `json:"channel"`
 	VirtualAccountConfig VirtualAccountConfig `json:"virtualAccountConfig"`
+	Origin               Origin               `json:"origin"`
 }
